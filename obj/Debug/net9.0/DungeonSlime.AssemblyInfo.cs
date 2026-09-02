76903d7bbc67308dbb5d52fc26a4e6f3c45b2f9a
Henry Wolf <HWolf.binz@gmail.com>
2026-09-02 23:48:02 +0200

    Add logo image and update content pipeline
    - Added logo image (images/logo.png) with texture processing parameters in Content.mgcb.
    - Generated logo.xnb for DesktopGL platform and added it to the appropriate directories.
    - Updated .mgcontent and .mgstats files to reflect the new logo asset.
    - Modified Game1.cs to load and display the logo in the game.
    - Updated project dependencies and assembly information.

--- obj/Debug/net9.0/DungeonSlime.AssemblyInfo.cs
+++ obj/Debug/net9.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6a377b36bc660ed6cabfb3a4b4a172aeb588b16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595d98bdf71d6e3111526dd0513818baef12a3c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
