595d98bdf71d6e3111526dd0513818baef12a3c9
Henry Wolf <HWolf.binz@gmail.com>
2026-09-02 23:30:10 +0200

    Add project references for MonoGameLibrary and update project files

--- obj/Debug/net9.0/DungeonSlime.AssemblyInfo.cs
+++ obj/Debug/net9.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+998f17cbaf1bebedd789f1e35023de7eaa259885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6a377b36bc660ed6cabfb3a4b4a172aeb588b16")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
